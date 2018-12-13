b29c0a87ba31570d304b2e49bb78feaa0ff93ecc
Daehyun You <daehyun.park.you@gmail.com>
2018-12-13 17:58:51 +0900

    Delete module vmitools.others

--- vmitools/__init__.py
+++ vmitools/__init__.py
@@ -4,5 +4,4 @@ from .histogram import Hist1d, Hist2d, Hist
 from .linear import LinearTransformer, SqueezeTransformer, opt_squ_pars
 from .abel import abel_inverse
 from .legendre import finite_legendre_transform_in_theta
-from .others import get_mask, get_avg
 from .asym import asym
