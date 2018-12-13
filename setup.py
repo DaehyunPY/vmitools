5baf8857af213723c0aedf036dfe3e988685be68
Daehyun You <daehyun.park.you@gmail.com>
2018-12-13 15:10:39 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='vmitools',
+    version='v201812.0',
+    author='Daehyun You',
+    author_email='daehyun@dc.tohoku.ac.jp',
+    url='https://github.com/DaehyunPY/vmitools',
+    # description='',
+    # long_description='',
+    license='MIT',
+    packages=[
+        'vmitools',
+    ],
+    install_requires=[
+        'numpy',
+        'numba',
+        'cytoolz',
+        'pandas',
+        'sympy',
+        'h5py',
+    ],
+)
