306dc9d5fcb52a6dc9b254cc9369b545d65618fc
Daehyun You <daehyun.park.you@gmail.com>
2018-12-14 12:42:57 +0100

    Delete module vmitools.histogram

--- vmitools/__init__.py
+++ vmitools/__init__.py
@@ -1,5 +1,4 @@
 from .coordinate import *
-from .histogram import *
 from .linear import *
 from .abel import *
 from .legendre import *
@@ -10,8 +9,6 @@ __all__ = [
     'convert_rth2xy',
     'convert_df_xy2rth',
     'convert_df_rth2xy',
-    'Hist1d',
-    'Hist2d',
     'LinearTransformer',
     'SqueezeTransformer',
     'opt_squ_pars',
