336097ebd2e450da4ebd252a566d781b3c380954
Daehyun You <daehyun.park.you@gmail.com>
2018-12-14 12:43:42 +0100

    Delete module vmitools.coordinate

--- vmitools/__init__.py
+++ vmitools/__init__.py
@@ -1,13 +1,8 @@
-from .coordinate import *
 from .abel import *
 from .legendre import *
 
 
 __all__ = [
-    'convert_xy2rth',
-    'convert_rth2xy',
-    'convert_df_xy2rth',
-    'convert_df_rth2xy',
     'abel_inverse',
     'finite_legendre_transform_in_theta',
 ]
